9eea9b1ba91f10d77008fafe01654ce31fe63137
rodrigorar <rodrigorar.dev@gmail.com>
2024-10-02 22:09:17 +0100

    Add help documentation

--- olisipo/cli.py
+++ olisipo/cli.py
@@ -8,7 +8,7 @@ from olisipo.operations import Operations
 
 class CLI(cmd.Cmd):
     prompt = ">> "
-    intro = "Hello, welcome to Olisipo"
+    intro = "Hello, welcome to Olisipo, a local simple secrets manager"
 
     def __init__(self, operations: Operations):
         super().__init__()
@@ -16,18 +16,29 @@ class CLI(cmd.Cmd):
         self.operations = operations
 
     def do_add(self, line):
+        'Add new secret with (add <entry-name> <entry-secret>)'
+
         split_input = line.split()
         self.operations.add_entry(Secret(name=split_input[0], value=split_input[1]))
 
     def do_get(self, line):
-        secret_value = self.operations.get_entry(secret_id=line)
-        print(secret_value.value)
+        'Get secret with (get <entry-name>)'
+
+        try:
+            secret_value = self.operations.get_entry(secret_id=line)
+            print(secret_value.value)
+        except:
+            print('Failed to fetch secret')
 
     def do_remove(self, line):
+        'Remove entry with (remove <entry-name>)'
+
         self.operations.remove_entry(line)
 
     @staticmethod
     def do_q(line):
+        'Quit the application'
+
         return True
 
 
