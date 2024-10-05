e89c5b30f14a0940eb372e9ed9ebc7014c191586
rodrigorar <rodrigorar.dev@gmail.com>
2024-10-06 00:29:27 +0100

    Add run script to installer

--- olisipo/bin/installer.sh
+++ olisipo/bin/installer.sh
@@ -4,11 +4,13 @@ OLISIPO_VERSION="1.0.0"
 install() {
   echo "Installing Olisipo $OLISIPO_VERSION"
   python3 -m pip install olisipo-$OLISIPO_VERSION-py3-none-any.whl --break-system-packages
+  mv olisipo.sh /usr/bin
 }
 
 uninstall() {
   echo "Removing Olisipo $OLISIPO_VERSION"
   python3 -m pip uninstall olisipo-$OLISIPO_VERSION-py3-none-any.whl --break-system-packages
+  rm /usr/bin/olisipo.sh
 }
 
 if [[ "$1" == "install" ]];
