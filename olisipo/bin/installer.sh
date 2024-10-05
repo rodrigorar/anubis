1baf14a3d4ccd8b4914e133277331d6bb13b8876
rodrigorar <rodrigorar.dev@gmail.com>
2024-10-06 00:37:29 +0100

    Add chmod and sudo configs to the installer

--- olisipo/bin/installer.sh
+++ olisipo/bin/installer.sh
@@ -1,16 +1,24 @@
 
+# FIXME: This should come from the pyproject.toml file
 OLISIPO_VERSION="1.0.0"
+
+help() {
+  echo "Not implemented"
+}
 
 install() {
   echo "Installing Olisipo $OLISIPO_VERSION"
   python3 -m pip install olisipo-$OLISIPO_VERSION-py3-none-any.whl --break-system-packages
-  mv olisipo.sh /usr/bin
+  chmod +x olisipo.sh
+  sudo mv olisipo.sh /usr/bin
+  # TODO Set the script such that we can call olisipo only and not olisipo.sh
 }
 
 uninstall() {
   echo "Removing Olisipo $OLISIPO_VERSION"
   python3 -m pip uninstall olisipo-$OLISIPO_VERSION-py3-none-any.whl --break-system-packages
-  rm /usr/bin/olisipo.sh
+  sudo rm /usr/bin/olisipo.sh
+  # TODO Remove the config to run olisipo.sh as olisipo
 }
 
 if [[ "$1" == "install" ]];
@@ -20,5 +28,5 @@ elif [[ "$1" == "remove" ]];
 then
   uninstall
 else
-  echo "Unknown command, please try again"
+  help
 fi
