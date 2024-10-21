c08ce6c770635cae09445aa6ae666487c4b0fd32
rodrigorar <rodrigorar.dev@gmail.com>
2024-10-21 21:43:54 +0100

    Fix install and uninstall processes

--- olisipo/bin/installer.sh
+++ olisipo/bin/installer.sh
@@ -1,11 +1,12 @@
 
-# FIXME: This should come from the pyproject.toml file
 OLISIPO_VERSION="1.0.0"
+OLISIPO_EXEC_LOCATION="/usr/local/bin"
+OLISIPO_EXEC="olisipo.sh"
 
 help() {
-  echo -e "\033[92m install\033[0m -> Install the Olisipo application"
-  echo -e "\033[92m uninstall\033[0m -> Uninstall the Olisipo application"
-  echo -e "\033[92m help\033[0m -> Shows this command"
+  echo "\033[92m install\033[0m -> Install the Olisipo application"
+  echo "\033[92m uninstall\033[0m -> Uninstall the Olisipo application"
+  echo "\033[92m help\033[0m -> Shows this command"
 }
 
 install() {
@@ -13,20 +14,20 @@ install() {
   python3 -m pip install olisipo-$OLISIPO_VERSION-py3-none-any.whl --break-system-packages
   chmod +x olisipo.sh
   if [[ ! -d /usr/local/bin ]]; then sudo mkdir /usr/local/bin; fi
-  sudo mv olisipo.sh /usr/local/bin
+  sudo mv $OLISIPO_EXEC $OLISIPO_EXEC_LOCATION
 }
 
 uninstall() {
   echo "Removing Olisipo $OLISIPO_VERSION"
   python3 -m pip uninstall olisipo-$OLISIPO_VERSION-py3-none-any.whl --break-system-packages
   rm -r $HOME/.olisipo
-  sudo rm /usr/bin/olisipo.sh
+  sudo rm $OLISIPO_EXEC_LOCATION/$OLISIPO_EXEC
 }
 
 if [[ "$1" == "install" ]];
 then
   install
-elif [[ "$1" == "remove" ]];
+elif [[ "$1" == "uninstall" ]];
 then
   uninstall
 else
