66f2b5961c3ce6803341054a021e6ef40fb48ae6
rodrigorar <rodrigorar.dev@gmail.com>
2024-10-21 21:31:28 +0100

    Improve installer script

--- olisipo/bin/installer.sh
+++ olisipo/bin/installer.sh
@@ -3,22 +3,24 @@
 OLISIPO_VERSION="1.0.0"
 
 help() {
-  echo "Not implemented"
+  echo -e "\033[92m install\033[0m -> Install the Olisipo application"
+  echo -e "\033[92m uninstall\033[0m -> Uninstall the Olisipo application"
+  echo -e "\033[92m help\033[0m -> Shows this command"
 }
 
 install() {
   echo "Installing Olisipo $OLISIPO_VERSION"
   python3 -m pip install olisipo-$OLISIPO_VERSION-py3-none-any.whl --break-system-packages
   chmod +x olisipo.sh
-  sudo mv olisipo.sh /usr/bin
-  # TODO Set the script such that we can call olisipo only and not olisipo.sh
+  if [[ ! -d /usr/local/bin ]]; then sudo mkdir /usr/local/bin; fi
+  sudo mv olisipo.sh /usr/local/bin
 }
 
 uninstall() {
   echo "Removing Olisipo $OLISIPO_VERSION"
   python3 -m pip uninstall olisipo-$OLISIPO_VERSION-py3-none-any.whl --break-system-packages
+  rm -r $HOME/.olisipo
   sudo rm /usr/bin/olisipo.sh
-  # TODO Remove the config to run olisipo.sh as olisipo
 }
 
 if [[ "$1" == "install" ]];
